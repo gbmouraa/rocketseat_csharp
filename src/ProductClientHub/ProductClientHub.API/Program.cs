15901bace33beafc29ab7ee29bb931693264f00e
gbmouraa <gabrielmoura-cma@hotmail.com>
2026-03-07 20:37:24 -0300

    chore: add configuração para abrir o swagger ao rodar o projeto

--- src/ProductClientHub/ProductClientHub.API/Program.cs
+++ src/ProductClientHub/ProductClientHub.API/Program.cs
@@ -3,8 +3,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -14,7 +12,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
