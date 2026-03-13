51139e90d715e6a9e20f7497a0afa8213b7ce9fe
gbmouraa <gabrielmoura-cma@hotmail.com>
2026-03-12 22:14:08 -0300

    chore: configura o Entity Framework

---
+++ src/ProductClientHub/ProductClientHub.API/Infrastructure/ProductClientHubDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductClientHub.API.Entities;
+
+namespace ProductClientHub.API.Infrastructure
+{
+    public class ProductClientHubDbContext : DbContext
+    {
+        // DbSet deve ser de um tipo de uma entidade / o nome sempre é o nome da tabela
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=C:\\snaps\\ProductClientHubDB.octet-stream");
+        }
+
+    }
+}
