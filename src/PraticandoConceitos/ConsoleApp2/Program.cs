336c149b4c573cff9ba0df44b012d64388acff7e
Gabriel Moura <gabrielm.reis@outlook.com>
2026-04-03 20:18:27 -0300

    feat: add Desafio 2

---
+++ src/PraticandoConceitos/ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Crie um programa que concatene um nome e um sobrenome inseridos pelo usuário e ao final exiba o nome completo.
+
+Desafio.Execute();
+
+public static class Desafio
+{
+    public static void Execute()
+    {
+        Console.Write("Digite o seu nome: ");
+        var nome = Console.ReadLine();
+
+        Console.Write("Digite o seu sobrenome: ");
+        var sobrenome = Console.ReadLine();
+
+        Console.WriteLine($"Nome completo: {nome} {sobrenome}");
+    }
+}
