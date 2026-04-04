199978b413562457d4e77dd91f537d3a4daa439b
Gabriel Moura <gabrielm.reis@outlook.com>
2026-04-03 21:17:49 -0300

    feat: add Desafio 4

---
+++ src/PraticandoConceitos/ConsoleApp4/Program.cs
@@ -0,0 +1,26 @@
+﻿//Crie um programa em que o usuário digita uma ou mais palavras e é exibido
+//a quantidadede caracteres que a palavra inserida tem.
+
+Desafio.Execute();
+
+public static class Desafio
+{
+    public static void Execute()
+    {
+        Console.Write("Digite uma ou mais palavras: ");
+        string[] palavras = Console.ReadLine().Trim().Split(" ");
+
+        Contador(palavras);
+    }
+
+    private static void Contador(string[] strings)
+    {
+        Dictionary<string, int> palavras = new Dictionary<string, int>();
+
+        foreach (string str in strings)
+            palavras[str] = str.Length;
+
+        foreach (var item in palavras)
+            Console.WriteLine($"{item.Key}: {item.Value} caracteres");
+    }
+}
