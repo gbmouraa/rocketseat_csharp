17f861bbc726b133a215c2e4eae1dec996c91cd3
Gabriel Moura <gabrielm.reis@outlook.com>
2026-04-16 22:37:48 -0300

    feat: add Requests e Responses; cria PetController

---
+++ src/Petfolio/Petfolio.API/Controllers/PetController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Petfolio.Communication.Requests;
+using Petfolio.Communication.Responses;
+
+namespace Petfolio.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PetController : ControllerBase
+    {
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponsePetJson), StatusCodes.Status201Created)]
+        public ActionResult Register([FromBody] RequestPetJson request)
+        {
+            return Created();
+        }
+    }
+}
---
+++ src/Petfolio/Petfolio.Communication/Responses/ResponsePetJson.cs
@@ -0,0 +1,8 @@
+﻿namespace Petfolio.Communication.Responses
+{
+    public class ResponsePetJson
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
