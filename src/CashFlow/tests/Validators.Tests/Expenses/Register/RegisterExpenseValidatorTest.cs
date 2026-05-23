b696ee54cf594737080b99365aabfc1d8acdcded
Gabriel Moura <gabrielm.reis@outlook.com>
2026-05-23 17:23:58 -0300

    feat: add RegisterExpenseValidatorTest

---
+++ src/CashFlow/tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTest.cs
@@ -0,0 +1,31 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
+
+namespace Validators.Tests.Expenses.Register
+{
+    public class RegisterExpenseValidatorTest
+    {
+        [Fact]
+        public void Success()
+        {
+            // Arrange
+            var validator = new RegisterExpenseValidator();
+
+            var expenseRequest = new RequestRegisterExpenseJson
+            {
+                Amount = 100,
+                Date = DateTime.Now.AddDays(-1),
+                Description = "Some description here",
+                PaymentType = EnumPaymentType.CreditCard,
+                Title = "Apple",
+            };
+
+            // Act
+            var result = validator.Validate(expenseRequest).IsValid;
+
+            // Assert
+            Assert.True(result);
+        }
+    }
+}
