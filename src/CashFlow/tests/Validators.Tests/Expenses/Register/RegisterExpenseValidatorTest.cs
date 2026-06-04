a833b4f5467d6847fdc1c1dfaec3b41794daaa72
Gabriel Moura <gabrielm.reis@outlook.com>
2026-06-04 12:05:22 -0300

    feat: add Error_Title_Empty test

--- src/CashFlow/tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTest.cs
+++ src/CashFlow/tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTest.cs
@@ -1,6 +1,8 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Exceptions;
 using CommonTestUtils;
 using Shouldly;
+using System.Collections.Frozen;
 
 namespace Validators.Tests.Expenses.Register
 {
@@ -14,10 +16,27 @@ namespace Validators.Tests.Expenses.Register
             var expenseRequest = RequestRegisterExpenseJsonBuilder.Build();
 
             // Act
-            var result = validator.Validate(expenseRequest).IsValid;
+            var result = validator.Validate(expenseRequest);
 
             // Assert
-            result.ShouldBeTrue();
+            result.IsValid.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void Error_Title_Empty()
+        {
+            // Arrange
+            var validator = new RegisterExpenseValidator();
+            var expenseRequest = RequestRegisterExpenseJsonBuilder.Build();
+            expenseRequest.Title = string.Empty;
+
+            // Act
+            var result = validator.Validate(expenseRequest);
+
+            // Assert
+            result.IsValid.ShouldBeFalse();
+            result.Errors.ShouldHaveSingleItem();
+            result.Errors.First().ErrorMessage.ShouldBe(ResourceErrorMessages.TITLE_REQUIRED);
         }
     }
 }
