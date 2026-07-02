43d212d061a7a16615526db670e2adcd13b39421
Gabriel Moura <gabrielm.reis@outlook.com>
2026-07-01 22:17:09 -0300

    feat: add ReportController

---
+++ src/CashFlow/src/CashFlow.Api/Controllers/ReportController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+
+namespace CashFlow.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : Controller
+    {
+        [HttpGet("excel")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult GetExcel([FromHeader] DateOnly month)
+        {
+            byte[] file = new byte[4];
+
+            if (file.Length > 0)
+                return File(file, MediaTypeNames.Application.Octet, "report.xlsx");
+
+            return NoContent();
+        }
+    }
+}
