2fd092459252e0bf0b83dc857241b0c5c96f0842
Gabriel Moura <gabrielm.reis@outlook.com>
2026-08-20 19:39:45 -0300

    feat: Execução automática de migrations no startup

--- src/CashFlow/src/CashFlow.Api/Program.cs
+++ src/CashFlow/src/CashFlow.Api/Program.cs
@@ -2,6 +2,7 @@ using CashFlow.Api.Filters;
 using CashFlow.Api.Middleware;
 using CashFlow.Application;
 using CashFlow.Infrastructure.DataAccess;
+using CashFlow.Infrastructure.Migrations;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -36,4 +37,17 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+await MigrateDataBase();
+
 app.Run();
+
+async Task MigrateDataBase()
+{
+    // escopo de DI
+    await using var scope = app.Services.CreateAsyncScope();
+    await DataBaseMigration.Migrate(scope.ServiceProvider);
+}
+
+//As migrations do Entity Framework Core não passam por nenhum endpoint HTTP. Por isso, não podemos depender do fluxo normal de uma requisição para obter o DbContext através da injeção de dependência.
+
+//Para executar as migrations durante a inicialização da aplicação, criamos manualmente um escopo de serviços (IServiceScope). A partir desse escopo, conseguimos acessar o CashFlowDbContext registrado no container de injeção de dependência.
---
+++ src/CashFlow/src/CashFlow.Infrastructure/Migrations/DataBaseMigration.cs
@@ -0,0 +1,17 @@
+﻿using CashFlow.Infrastructure.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace CashFlow.Infrastructure.Migrations
+{
+    public static class DataBaseMigration
+    {
+        //O MigrateAsync() verifica quais migrations ainda não foram aplicadas e as executa no banco de dados.
+        public static async Task Migrate(IServiceProvider serviceProvider)
+        {
+            var dbContext = serviceProvider.GetRequiredService<CashFlowDbContext>();
+            await dbContext.Database.MigrateAsync();
+        }
+    }
+}
